28f3e70b72659d1cf58e2c699434c7651e1e1f1d
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 20:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var  newArray = [];
+  for (let i = 0; i < musicians.length; i++){
+    if (i === 0) {
+      newArray.push(`${musicians[0]} plays ${instruments[0]}`);
+    } else {
+      newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  }
+  return newArray;
+}
